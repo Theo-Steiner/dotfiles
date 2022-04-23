7064d6490c7849effacbfff5e12d0fb525ee0387
Theo Steiner <theosteiner@gmail.com>
2022-04-23 13:41:31 +0900

    Always show signcol

--- .config/nvim/lua/config/appearance.lua
+++ .config/nvim/lua/config/appearance.lua
@@ -4,22 +4,12 @@
 
 -- enable all the colors
 Set.termguicolors = true
+Set.signcolumn = "yes"
 
 -- make ~ chars disappear
 Set.fillchars = "eob: "
 
 -- Font that makes Coc-Explorer look nice and adds Powerline icons
 Set.guifont = "Hack Nerd Font"
--- Highlight the line currently selected by making the text bold
+-- Highlight cursor line
 Set.cursorline = true
-
-require("warped").setup()
-
-vim.cmd([[
-    hi CursorLine guibg=NONE gui=bold
-    hi Folded guibg=NONE guifg=#fefefe
-    hi Normal guifg=NONE guibg=NONE
-    hi VertSplit gui=NONE guibg=NONE
-    hi LineNr guibg=NONE guifg=NONE
-    hi SignColumn guibg=NONE
-]])
