65ef5cb15b9149aae195dbafc23a7da83d89c97f
Theo Steiner <theosteiner@gmail.com>
2022-04-24 12:09:32 +0900

    Silence autocommand to restore views

--- .config/nvim/lua/config/basics.lua
+++ .config/nvim/lua/config/basics.lua
@@ -37,7 +37,7 @@ vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 	pattern = { "*.*" },
 	callback = function()
-		vim.cmd([[silent loadview]])
+		vim.cmd([[silent! loadview]])
 	end,
 })
 
