9104537888b4a691ac4d66572d596ccf45ae165c
Theo Steiner <theosteiner@gmail.com>
2022-04-24 14:54:47 +0900

    change solarized highlight color

--- .config/nvim/lua/config/_warped.lua
+++ .config/nvim/lua/config/_warped.lua
@@ -1,5 +1,6 @@
 local mapping = require("warped.default_mapping")
 mapping["gruvboxdark"] = require("warped.utils").shallow_copy(mapping.default)
+mapping["solarizeddark"] = mapping["gruvboxdark"]
 mapping.gruvboxdark.blue = "bright_magenta"
 require("warped").setup({
 	theme_config = function(Color, colors, Group, groups, styles)
