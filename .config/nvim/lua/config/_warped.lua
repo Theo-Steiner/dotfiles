b55791f04cd4b57cd447e5a9595b179c04c0e51a
Theo Steiner <theosteiner@gmail.com>
2022-06-14 15:10:28 +0900

    Prettify telescope floating window

--- .config/nvim/lua/config/_warped.lua
+++ .config/nvim/lua/config/_warped.lua
@@ -7,6 +7,27 @@ require("warped").setup({
 		require("warped.default_theme_config")(Color, colors, Group, groups, styles)
 		Group.new("CursorLine", colors.none, colors.none, styles.bold)
 		Group.new("Folded", colors.none, colors.none)
+
+		local darker = colors.background:dark(0.01)
+		local lighter = colors.background:light()
+
+		-- Telescope prompt
+		Group.new("TelescopePromptTitle", colors.black, colors.red)
+		Group.new("TelescopePromptBorder", colors.none, lighter)
+		Group.new("TelescopePromptNormal", colors.blue, lighter)
+		Group.new("TelescopePromptPrefix", colors.red, lighter)
+
+		-- Telescope results
+		Group.new("TelescopeResultsTitle", colors.none, darker)
+		Group.new("TelescopeResultsBorder", colors.none, lighter)
+		Group.new("TelescopeResultsNormal", colors.none, darker)
+		-- selected result hl group
+		Group.new("TelescopeSelection", colors.black, colors.red)
+
+		-- Telescope preview
+		Group.new("TelescopePreviewTitle", colors.none, colors.none)
+		Group.new("TelescopePreviewBorder", colors.none, lighter)
+		Group.new("TelescopePreviewNormal", colors.none, darker)
 	end,
 	mapping = mapping,
 })
