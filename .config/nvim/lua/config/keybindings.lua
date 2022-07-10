7f629e45735bd22535591b3e9dcde968e6a74050
Theo Steiner <theosteiner@gmail.com>
2022-07-10 16:41:31 +0900

    Toggle comment ugly fix

--- .config/nvim/lua/config/keybindings.lua
+++ .config/nvim/lua/config/keybindings.lua
@@ -15,7 +15,7 @@ Map("n", "<leader>fs", "<cmd>Telescope git_status<cr>")
 -- <space> c toggles the current line or current selection to be a comment
 Map("", "<leader>c", function()
 	require('ts_context_commentstring.internal').update_commentstring()
-	vim.api.nvim_command('Commentary')
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(":Commentary<CR>", true, false, true), "", false)
 end
 )
 
@@ -24,20 +24,6 @@ Map("n", "<leader>pv", ":Neotree reveal toggle<cr>")
 -- open git status neotree float
 Map("n", "<leader>pg", ":Neotree float git_status<cr>")
 
--- LSP actions
--- show information about code at cursor position
-Map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
---
-Map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
-Map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>")
-Map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")
-Map("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
-Map("n", "gr", "<cmd>TroubleToggle lsp_references<cr>")
-Map("n", "s", "<cmd>lua vim.lsp.buf.rename()<cr>")
-Map("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<cr>")
-
--- Diagnostics
-Map("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
 
 -- Function using buffernames to see if "Trouble" is open
 local check_trouble_state = function()
